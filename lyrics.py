507f9c4a661e512bc59796dc71c3a594abcd1181
jeevandas-jd <jeevandasms2004@gmail.com>
2025-03-04 00:14:08 +0530

    fix

--- lyrics.py
+++ lyrics.py
@@ -1,24 +1,53 @@
 import lyricsgenius as lg
+import os
+from dotenv import load_dotenv
 
-genius =lg.Genius("ecgGNpmLy4EXajEKfSuFRfC4dtLeT3fXCScRqJCazw1W2W2rVW9zDJDSydKUcbew")
+load_dotenv()
+
+
+api_key = os.getenv("GENIUS_API_TOKEN")
+
+if not api_key:
+    print("API token not found. Please check your .env file.")
+    exit()
+
+
+genius = lg.Genius(api_key)
+
 while True:
-    input_artist=input("Enter the name of the artist: ")
+    input_artist = input("Enter the name of the artist: ")
 
+    # Search for the artist and get up to 5 songs
     artist = genius.search_artist(input_artist, max_songs=5)
 
-    songs = artist.songs
-    for i, song in enumerate(songs):
-        print(f"{i+1}. {song.title}")
+    if artist:
+        songs = artist.songs
 
-    is_lyrics = input("Do you want to see the lyrics of any of these songs? (y/n): ")
+        # Display the list of songs
+        for i, song in enumerate(songs):
+            print(f"{i + 1}. {song.title}")
 
-    if is_lyrics == 'y':
-        song_number = int(input("Enter the song number: "))
-        print(songs[song_number-1].lyrics)
-    else:    
-        print("Thank you for using this program!")
-    ask=input("Do you want to search for another artist? (y/n): ")
-    if ask=='n':
-        break
+        # Ask if the user wants to see lyrics
+        is_lyrics = input("Do you want to see the lyrics of any of these songs? (y/n): ")
+
+        if is_lyrics.lower() == 'y':
+            try:
+                song_number = int(input("Enter the song number: "))
+                if 1 <= song_number <= len(songs):
+                    print(f"\nLyrics of {songs[song_number - 1].title}:\n")
+                    print(songs[song_number - 1].lyrics)
+                else:
+                    print("Invalid song number.")
+            except ValueError:
+                print("Please enter a valid number.")
+
+        else:
+            print("Thank you for using this program!")
     else:
-        continue
+        print(f"No results found for artist: {input_artist}")
+
+    # Ask if the user wants to search for another artist
+    ask = input("Do you want to search for another artist? (y/n): ")
+    if ask.lower() == 'n':
+        print("Goodbye!")
+        break
